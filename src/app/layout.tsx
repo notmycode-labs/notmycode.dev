faf1a7d08b0566397f0f7f825de8ea126066fa62
bluestar_ <canikissfemboy@gmail.com>
2024-02-17 19:07:50 +0700

    Fix import path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 
 import "./globals.css";
 import { Roboto } from "next/font/google";
-import Footer from "./components/Footer";
+import Footer from "@/components/Footer";
 
 const font = Roboto({
   subsets: ["latin"],
