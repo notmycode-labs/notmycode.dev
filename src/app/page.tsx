a4f3e7eef1d687f8b7a2e6640135f95f0c2007f9
bluestar_ <canikissfemboy@gmail.com>
2024-02-19 04:25:24 +0700

    Code formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
-import { Title } from "@/components/Title";
-import { Roboto_Mono } from "next/font/google";
-import { TypeAnimation } from 'react-type-animation';
+import { Title } from "@/components/Title"
+import { Roboto_Mono } from "next/font/google"
+import { TypeAnimation } from "react-type-animation"
 const descFont = Roboto_Mono({
   subsets: ["latin"],
   weight: "400",
-});
+})
 
 export default function Home() {
   return (
     <main className="flex justify-center h-screen items-center w-full py-6 md:py-12 lg:py-16 p-2">
       <div className="">
-
         <h2 className="title text-4xl text-center p-4 font-bold tracking-tighter underline xl:text-5xl">
-        <Title/>
+          <Title />
         </h2>
 
         <p className={`max-w-3xl text-center ${descFont.className}`}>
@@ -21,5 +20,5 @@ export default function Home() {
         </p>
       </div>
     </main>
-  );
+  )
 }
