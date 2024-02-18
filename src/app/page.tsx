180af88dd7fe3018a235aa0c5e03912044c35321
bluestar_ <canikissfemboy@gmail.com>
2024-02-19 04:06:19 +0700

    Unknown update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
+import { Title } from "@/components/Title";
 import { Roboto_Mono } from "next/font/google";
-
+import { TypeAnimation } from 'react-type-animation';
 const descFont = Roboto_Mono({
   subsets: ["latin"],
   weight: "400",
@@ -9,8 +10,9 @@ export default function Home() {
   return (
     <main className="flex justify-center h-screen items-center w-full py-6 md:py-12 lg:py-16 p-2">
       <div className="">
+
         <h2 className="title text-4xl text-center p-4 font-bold tracking-tighter underline xl:text-5xl">
-          notmycode.dev
+        <Title/>
         </h2>
 
         <p className={`max-w-3xl text-center ${descFont.className}`}>
