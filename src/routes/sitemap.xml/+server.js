bc38392f962fff2d06fd4c905e7dbdbd4288ea9b
blue <canikissfemboy@gmail.com>
2024-10-13 13:23:27 +0700

    add sitemap

---
+++ src/routes/sitemap.xml/+server.js
@@ -0,0 +1,45 @@
+import fs from 'fs';
+import path from 'path';
+
+const site = 'https://notmycde.dev';
+const staticPages = ['projects/', 'about/'];
+
+async function fetchBlogArticles() {
+	const blogDirectory = path.resolve('contents');
+	const filenames = fs.readdirSync(blogDirectory);
+
+	return filenames.filter((file) => file.endsWith('.md')).map((file) => file.replace('.md', ''));
+}
+
+export async function GET({ url }) {
+	const blogPages = await fetchBlogArticles();
+	const pages = [...staticPages, ...blogPages.map((slug) => `blog/${slug}/`)];
+
+	const body = sitemap(site, pages);
+	const response = new Response(body);
+	response.headers.set('Cache-Control', 'max-age=0, s-maxage=3600');
+	response.headers.set('Content-Type', 'application/xml');
+	return response;
+}
+
+const sitemap = (site, pages) => `<?xml version="1.0" encoding="UTF-8" ?>
+<urlset
+  xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+  xmlns:xhtml="https://www.w3.org/1999/xhtml"
+  xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+  xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+  xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+>
+  ${pages
+		.map(
+			(page) => `
+<url>
+<loc>${site}${page}</loc>
+<changefreq>daily</changefreq>
+<priority>0.5</priority>
+</url>
+`
+		)
+		.join('')}
+</urlset>`;
