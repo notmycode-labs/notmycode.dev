e8c7b523a6f794ff79ca67e5f4b25073241ec1f5
blue <canikissfemboy@gmail.com>
2024-10-13 13:32:11 +0700

    formatting

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -1,26 +1,20 @@
-const site = "https://notmycde.dev";
-const staticPages = [
-  "projects/",
-  "about/"
-];
+const site = 'https://notmycde.dev';
+const staticPages = ['projects/', 'about/'];
 
 function fetchBlogArticles() {
-  const blogFiles = import.meta.glob('/contents/*.md');
-  return Object.keys(blogFiles).map(file => file.replace('/contents/', '').replace('.md', ''));
+	const blogFiles = import.meta.glob('/contents/*.md');
+	return Object.keys(blogFiles).map((file) => file.replace('/contents/', '').replace('.md', ''));
 }
 
 export async function GET({ url }) {
-  const blogPages = fetchBlogArticles();
-  const pages = [
-    ...staticPages,
-    ...blogPages.map(slug => `blog/${slug}/`)
-  ];
+	const blogPages = fetchBlogArticles();
+	const pages = [...staticPages, ...blogPages.map((slug) => `blog/${slug}/`)];
 
-  const body = sitemap(site, pages);
-  const response = new Response(body);
-  response.headers.set("Cache-Control", "max-age=0, s-maxage=3600");
-  response.headers.set("Content-Type", "application/xml");
-  return response;
+	const body = sitemap(site, pages);
+	const response = new Response(body);
+	response.headers.set('Cache-Control', 'max-age=0, s-maxage=3600');
+	response.headers.set('Content-Type', 'application/xml');
+	return response;
 }
 
 const sitemap = (site, pages) => `<?xml version="1.0" encoding="UTF-8" ?>
@@ -33,14 +27,14 @@ const sitemap = (site, pages) => `<?xml version="1.0" encoding="UTF-8" ?>
   xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
 >
   ${pages
-    .map(
-      page => `
+		.map(
+			(page) => `
 <url>
 <loc>${site}${page}</loc>
 <changefreq>daily</changefreq>
 <priority>0.5</priority>
 </url>
 `
-    )
-    .join("")}
+		)
+		.join('')}
 </urlset>`;
