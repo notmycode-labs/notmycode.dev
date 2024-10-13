b26042fb14156ef0821cedf58df1ccae668dd15d
blue <canikissfemboy@gmail.com>
2024-10-13 22:02:00 +0700

    hell nah wrong commit

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,5 @@
 <p>
-	We are a group of software and hardware devs, founded in mid-2024 by blueskychan_, we fuck around
+	We are a group of software and hardware devs, founded in 2023-12-18 by blueskychan_, we fuck around
 	and messed up shits more than /dev/random does.
 </p>
 <blockquote>group name proposed by bluestar.meow</blockquote>
