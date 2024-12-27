7da288e36f1d5c5d2ea35f9aa3a1aa5665974adf
blue <canikissfemboy@gmail.com>
2024-12-27 22:11:40 +0700

    overflow y

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -32,7 +32,7 @@
 />
 
 <article
-	class="prose md:prose-lg lg:prose-3xl p-2 prose-h1:text-3xl prose-invert prose-neutral mx-auto h-screen prose-img:rounded-xl prose-video:rounded-lg"
+	class="prose overflow-y-scroll md:prose-lg lg:prose-3xl p-2 prose-h1:text-3xl prose-invert prose-neutral mx-auto h-screen prose-img:rounded-xl prose-video:rounded-lg"
 >
 	<a class="not-prose" href="/blog">Return to /blog</a>
 	{@html data.content}
