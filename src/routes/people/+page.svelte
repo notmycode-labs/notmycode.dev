23e35a9dcfcfaefc81d9f25176ba6d37542f7270
blue <canikissfemboy@gmail.com>
2025-02-04 12:14:53 +0700

    now fuck off

--- src/routes/people/+page.svelte
+++ src/routes/people/+page.svelte
@@ -6,7 +6,7 @@
 	{#each data.data as user}
 		<div class="border p-2 rounded-md mt-1.5 hover:border-pink-700 border-pink-300 w-full">
 			<h2 class="font-bold text-lg">
-				<a href={`/people/${user.name.toLowerCase()}`}>{user.name}</a>
+				<a href={`/people/${user.file_name}`}>{user.name}</a>
 			</h2>
 			<p>IRL Name: {user.irl_name || 'N/A'}</p>
 			<p>Role: {user.role}</p>
